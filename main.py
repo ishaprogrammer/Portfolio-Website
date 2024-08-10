93b449d8d355cd820dd8e2369974879e7c4121f5
Taskin <159234201+ishaprogrammer@users.noreply.github.com>
2024-08-10 14:24:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/resume")
+def download_resume():
+    return render_template('resume.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
